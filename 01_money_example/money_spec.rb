cd2c4b4c49b636ced71d2775372925b1f9ec3422
n-ogawa <fy59.kou-729@live.jp>
2021-12-15 22:40:35 +0900

    最初のテストをグリーンにする

--- 01_money_example/money_spec.rb
+++ 01_money_example/money_spec.rb
@@ -4,7 +4,7 @@ require_relative './dollar'
 RSpec.describe Dollar do
   it 'multiplication' do
     five = Dollar.new(5)
-    five.times(5)
+    five.times(2)
     expect(five.amount).to eq 10
   end
 end
