023ca1dfb8d650d8e9dc3df15fa89371bdcd6894
n-ogawa <fy59.kou-729@live.jp>
2021-12-18 20:49:19 +0900

    Franc同士の計算を丸コピで実装

--- 01_money_example/money_spec.rb
+++ 01_money_example/money_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative './dollar'
+require_relative './franc'
 
 RSpec.describe Dollar do
   it 'multiplication' do
@@ -11,5 +12,11 @@ RSpec.describe Dollar do
   it 'equality' do
     expect(Dollar.new(5)).to eq Dollar.new(5)
     expect(Dollar.new(5)).not_to eq Dollar.new(6)
+  end
+
+  it 'franc multiplication' do
+    five = Franc.new(5)
+    expect(five.times(2)).to eq Franc.new(10)
+    expect(five.times(3)).to eq Franc.new(15)
   end
 end
