ebb128b11db1613dc31f18bb661b54075ab45aaa
n-ogawa <fy59.kou-729@live.jp>
2021-12-18 20:36:49 +0900

    timesがDollarを返すと伝わりやすいテストにする

--- 01_money_example/money_spec.rb
+++ 01_money_example/money_spec.rb
@@ -4,10 +4,8 @@ require_relative './dollar'
 RSpec.describe Dollar do
   it 'multiplication' do
     five = Dollar.new(5)
-    product = five.times(2)
-    expect(product.amount).to eq 10
-    product = five.times(3)
-    expect(product.amount).to eq 15
+    expect(five.times(2)).to eq Dollar.new(10)
+    expect(five.times(3)).to eq Dollar.new(15)
   end
 
   it 'equality' do
