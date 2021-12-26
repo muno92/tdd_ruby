d024e0bbd5fc374a2243820bc238f48198ae82a4
n-ogawa <fy59.kou-729@live.jp>
2021-12-26 21:52:44 +0900

    加算結果の積算を実装

--- 01_money_example/TODO.md
+++ 01_money_example/TODO.md
@@ -7,4 +7,4 @@
 - [x] ~~Moneyを変換して換算を行う~~
 - [x] ~~Reduce(Bank, String)~~
 - [x] ~~Sum.plus~~
-- [ ] Expression.times
+- [x] ~~Expression.times~~
--- 01_money_example/sum.rb
+++ 01_money_example/sum.rb
@@ -16,6 +16,10 @@ class Sum < Expression
     Sum.new(self , addend)
   end
 
+  def times(multiplier)
+    Sum.new(@augend.times(multiplier), @addend.times(multiplier))
+  end
+
   def reduce(bank, to)
     amount = @augend.reduce(bank, to).amount + addend.reduce(bank, to).amount
     Money.new(amount, to)
